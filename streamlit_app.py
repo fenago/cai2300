8cc1a0342f87552c84ede2f246af4f0804d4294e
Dr. Ernesto Lee <socrates73@gmail.com>
2024-02-16 16:57:48 -0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,19 @@
 import streamlit as st
+import pickle
 
-st.title('🎈 App Name')
+# Load the pickled model
+with open('nlp.pkl', 'rb') as file:
+    model = pickle.load(file)
 
-st.write('Hello world!')
+st.title('NLP Sentiment Analysis')
+
+# User input
+user_input = st.text_area("Enter Text", "")
+
+# Predict and display the result
+if st.button('Predict Sentiment'):
+    prediction = model.predict([user_input])[0]
+    if prediction == 1:
+        st.write('Positive Sentiment')
+    else:
+        st.write('Negative Sentiment')
